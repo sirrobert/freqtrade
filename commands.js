13ef20d16bce90c6115dc67e8bfe1948179519ca
Sir Robert Burbridge <sirrobert@gmail.com>
2021-09-25 10:57:15 -0400

    Adding some commands.

---
+++ commands.js
@@ -0,0 +1,5 @@
+const commands = {
+  "create-user"   : "freqtrade create-userdir --userdir user_data",
+  "create-config" : "freqtrade new-config --config config.json",
+  "start-trading" : "freqtrade trade --config config.json --strategy SampleStrategy",
+};
